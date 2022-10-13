948767b06837ac30757859752ae6a1788d096c66
Cristea Bogdan <cristea.ibogdan@gmail.com>
2022-10-13 14:48:26 +0300

    Remade AccountReader using ArrayList. If the user/pass is incorrect 3 times in a row the application exits.

---
+++ src/SecondIteration/customer/AccountReader.java
@@ -0,0 +1,87 @@
+package SecondIteration.customer;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class AccountReader {
+
+    public static void main(String[] args) {
+
+        ArrayList<Object> accList = new ArrayList<>();
+        boolean value = false;
+        int counter = 3;
+
+        while (value == false) {
+
+            Scanner reader = new Scanner(System.in);
+            System.out.println("->Account login process<-");
+
+            System.out.println("Type user: ");
+            String user = reader.nextLine();
+
+            System.out.println("Type pass: ");
+            String pass = reader.nextLine();
+
+            try (Scanner scanner = new Scanner(new FileInputStream("data.txt"))) {
+
+                while (scanner.hasNext()) {
+                    //1.Scan line
+                    String accountLine = scanner.nextLine();
+                    //System.out.println("Line read is: " + accountLine);
+
+                    //2.Break line in words, split by " " (empty string)
+                    String[] accountComp = accountLine.split("\\|");
+
+                    //2.1. Verify step 2 is working
+//                    for (int i = 0; i < accountComp.length; i++) {
+//                        System.out.println("\tComponenta " + i + " este " + accountComp[i]);
+//                    }
+
+                    //3.Create object of Account type using the above components
+                    Account account = new Account(accountComp[0], accountComp[1]);
+                    //System.out.println("Scanned account is: " + account);
+
+                    //4.Insert created objects in Account[] vector
+                    accList.add(account);
+                    //index++; nu mai este necesar
+                }
+
+            } catch (FileNotFoundException fe) {
+                System.out.println("File not found!");
+            }
+
+            for (Object o : accList) {
+                //System.out.println(accList.get(index));
+                Account objectOne = (Account) o;
+
+                if (objectOne.getUser().equals(user) && objectOne.getPass().equals(pass)) {
+                    System.out.println("Log in success!");
+                    value = true;
+                    break;
+                }
+                value = false;
+            }
+
+            if (value == false) {
+
+                if (counter > 1) {
+                    System.out.println("Invalid user or pass! You have " + counter + " more tries!");
+                }
+
+                if (counter == 1) {
+                    System.out.println("Invalid user or pass! You have " + counter + " more try!");
+                }
+
+                if (counter == 0) {
+                    System.out.println("Account temporarily blocked!");
+                    System.exit(1);
+                    break;
+                }
+                counter--;
+            }
+        }
+    }
+}
+
+
+
